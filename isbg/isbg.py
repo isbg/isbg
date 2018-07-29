e8c7c63326e7363993e8d6020151003dedcb39ab
Louis-Philippe Véronneau <pollito@riseup.net>
2018-07-29 15:37:19 +0800

    bump version to 2.1.1

--- isbg/isbg.py
+++ isbg/isbg.py
@@ -60,7 +60,7 @@ Note:
 """
 
 
-__version__ = "2.1.0"  #: The current isbg version
+__version__ = "2.1.1"  #: The current isbg version
 
 __license__ = \
     """License GPLv3: GNU GPL version 3 https://gnu.org/licenses/gpl.html
