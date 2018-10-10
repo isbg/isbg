003db279fc66bc4d4157aa6d51515b8f618fa3d2
Louis-Philippe Véronneau <pollito@riseup.net>
2018-10-10 12:55:47 -0400

    don't build the documentation when installing

--- setup.py
+++ setup.py
@@ -5,49 +5,9 @@
 import ast
 import os
 import re
-import subprocess
 import sys
-import fnmatch
 
-from distutils.command.build import build as _build
 from setuptools import setup
-
-
-class Build(_build):
-    """Customize setuptools build command."""
-
-    def run(self):
-        """Create the docs (html and manpage)."""
-        ret = subprocess.call(['make', 'docs'])
-        if ret != 0:
-            sys.stderr.write("Error in call to make doc")
-            sys.exit(ret)
-        _build.run(self)
-
-
-def find_data_files(base_dir, pattern, folder, recursive=True):
-    """Yield all the files matching the pattern."""
-    data_files = []
-
-    if recursive:
-        for path, folders, files in os.walk(folder):
-            flist = []
-            new_base_dir = os.path.join(base_dir,
-                                        os.path.relpath(path, folder))
-            for filename in fnmatch.filter(files, pattern):
-                fullname = os.path.join(path, filename)
-                if os.path.isfile(fullname):
-                    flist.append(fullname)
-            data_files.append((new_base_dir, flist))
-    else:
-        flist = []
-        for filename in fnmatch.filter(os.listdir(folder), pattern):
-            fullname = os.path.join(folder, filename)
-            if os.path.isfile(fullname):
-                flist.append(fullname)
-            data_files.append((base_dir, flist))
-
-    return data_files
 
 
 # We get the isbg README.rst as package long description:
@@ -82,17 +42,12 @@ setup(
             'isbg_sa_unwrap = isbg.sa_unwrap:isbg_sa_unwrap',
         ]
     },
-    cmdclass={
-        'build': Build,
-    },
-    data_files=find_data_files('share/man/man1/', '*', 'build/sphinx/man') + \
-    find_data_files('share/doc/isbg/html', '*', 'build/sphinx/html'),
     install_requires=['docopt'],
     extras_require={
         'chardet': ['chardet'],
         'cchardet': ['cchardet'],
     },
-    setup_requires=pytest_runner + ['sphinx_rtd_theme', 'sphinx'],
+    setup_requires=pytest_runner,
     tests_require=['pytest', 'mock', 'pytest-cov'],
     url='https://github.com/isbg/isbg',
     classifiers=[
